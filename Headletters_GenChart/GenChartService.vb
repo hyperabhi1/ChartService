39df4ed74b25c0fea09a027da52653f735b3285b
Kirti Vashishtha <kirti.vashishtha@enukesoftware.com>
2018-09-26 18:21:16 +0530

    Initials

---
+++ Headletters_GenChart/GenChartService.vb
@@ -0,0 +1,26 @@
+﻿Imports System.IO
+
+Public Class GenChartService
+    Dim timer As Timers.Timer
+    Protected Overrides Sub OnStart(ByVal args() As String)
+        System.Diagnostics.Debugger.Launch()
+        timer = New Timers.Timer()
+        timer.Interval = 60000
+        AddHandler timer.Elapsed, AddressOf TriggerGenChart
+        timer.Enabled = True
+    End Sub
+
+    Protected Overrides Sub OnStop()
+        timer.Enabled = False
+    End Sub
+
+    Private Sub TriggerGenChart(obj As Object, e As EventArgs)
+        Try
+            GenChart.Main()
+        Catch ex As Exception
+            Dim strFile As String = String.Format("C:\inetpub\wwwroot\ServiceLogs\ErrorLog_{0}.txt", DateTime.Today.ToString("dd-MMM-yyyy"))
+            File.AppendAllText(strFile, String.Format("Error Occured at-- {0}{1}{2}", DateTime.Now, Environment.NewLine, ex.Message + vbCrLf + ex.StackTrace))
+        End Try
+    End Sub
+
+End Class
---
+++ Headletters_GenChart/PersonalDetails.vb
@@ -0,0 +1,13 @@
+﻿Public Class PersonalDetails
+    Public Property NameoftheChartOwner As String
+    Public Property DateofBirth As String
+    Public Property PlaceofBirth As String
+    Public Property TimeofBirth As String
+    Public Property Latitude As String
+    Public Property Longitude As String
+    Public Property DayofBirth As String
+    Public Property Rashi As String
+    Public Property Star As String
+    Public Property SunSign As String
+    Public Property BalanceDasa As String
+End Class
